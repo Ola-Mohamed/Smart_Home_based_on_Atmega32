36d662af308505c0c5b3ec0afb9aa532e5831613
Ola mohamed <66176966+Ola-Mohamed@users.noreply.github.com>
2023-10-26 23:32:33 +0200

    Update Led.c

--- HAL/LED/Led.c
+++ HAL/LED/Led.c
@@ -12,25 +12,25 @@
 
 
 
-void LED_init(LED_config_t Led_config){
+void HAL_LED_init(LED_config_t Led_config){
 
 	MCAL_DIO_INIT(&Led_config->port_id,&Led_config->pin_id,&Led_config->pin_direction);
 
 }
 
-void LED_setOn(void)
+void HAL_LED_setOn(void)
 {
 	 MCAL_DIO_Std_WRITE_PIN_DIRECTION(LED_PORT,LED_PIN,LED_ON);  /* LED ON */
 }
 
 /*********************************************************************************************/
-void LED_setOff(void)
+void HAL_LED_setOff(void)
 {
 	MCAL_DIO_Std_WRITE_PIN_DIRECTION(LED_PORT,LED_PIN,LED_OFF); /* LED OFF */
 }
 
 /*********************************************************************************************/
-void LED_toggle(void)
+void HAL_LED_toggle(void)
 {
      MCAL_DIO_Std_FLIP_PIN_DIRECTION(LED_PORT,LED_PIN);
 }
