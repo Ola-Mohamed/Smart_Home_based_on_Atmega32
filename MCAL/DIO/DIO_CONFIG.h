8729c90ccf029e0413476289ea31668bc19264a3
Magdy Khalifa <148630573+engmagdykhalifa@users.noreply.github.com>
2023-10-26 22:23:02 +0300

    Update DIO_CONFIG.h

--- MCAL/DIO/DIO_CONFIG.h
+++ MCAL/DIO/DIO_CONFIG.h
@@ -8,6 +8,11 @@
 #ifndef DIO_CONFIG_H_
 #define DIO_CONFIG_H_
 
+#include "DIO_TYPES.h"
+
+
+extern const DIO_PIN_DIRECTION_TYPE pin_StatusArr[TOTALPORTS_ID][TOTALPINS_ID];
+
 
 
 #endif /* DIO_CONFIG_H_ */
