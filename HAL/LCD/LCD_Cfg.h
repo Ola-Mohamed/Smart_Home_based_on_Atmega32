8c7df052773659b6be6d70ceece38215d298f646
AhmedGamalEldesawy <148612288+AhmedGamalEldesawy@users.noreply.github.com>
2023-10-27 02:42:00 +0200

    Create LCD_Cfg.h

---
+++ HAL/LCD/LCD_Cfg.h
@@ -0,0 +1,26 @@
+
+#ifndef LCD_CFG_H_
+#define LCD_CFG_H_
+
+#define  _4_BIT   1
+#define  _8_BIT   2
+
+
+/* _4_BIT, _8_BIT*/
+#define  LCD_MODE  _4_BIT
+
+
+#define  RS   PINB7
+#define  EN   PINB1
+
+#define  LCD_PORT   PA
+
+#define  D7  PINA1
+#define  D6  PINB0
+#define  D5  PINC3
+#define  D4  PINA7
+
+
+
+
+#endif /* LCD_CFG_H_ */
