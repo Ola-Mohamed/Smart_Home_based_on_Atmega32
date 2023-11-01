46f260e2d373fa5aae9b9655ea13cd3522165071
AhmedGamalEldesawy <148612288+AhmedGamalEldesawy@users.noreply.github.com>
2023-11-01 16:50:49 +0200

    Add files via upload

---
+++ HAL/LCD/LCD_cfg.c
@@ -0,0 +1,10 @@
+#include "DIO_TYPES.h"
+
+Dio_ConfigType  RS = {PIN6_ID , PORTA_ID  }  ; 
+
+Dio_ConfigType  EN = {PIN7_ID , PORTA_ID}  ; 
+Dio_ConfigType  D4 = {PIN4_ID, PORTB_ID} ;  
+Dio_ConfigType  D5 = {PIN5_ID , PORTB_ID };
+Dio_ConfigType  D6 = {PIN6_ID ,  PORTB_ID} ;
+Dio_ConfigType  D7=  {PIN7_ID , PORTB_ID} ;  
+Dio_ConfigType LCD_PORT = {TOTALPINS_ID,PORTB_ID  }  ; 
