3fffdbc59346cc2f647a8aa6257a3c8322dd755f
AhmedGamalEldesawy <148612288+AhmedGamalEldesawy@users.noreply.github.com>
2023-11-01 16:49:56 +0200

    Update LCD_Cfg.h

--- HAL/LCD/LCD_Cfg.h
+++ HAL/LCD/LCD_Cfg.h
@@ -2,25 +2,29 @@
 #ifndef LCD_CFG_H_
 #define LCD_CFG_H_
 
+
+#include "MemMap.h"
+#include "DIO_TYPES.h"
+
+
+
 #define  _4_BIT   1
 #define  _8_BIT   2
+
+
 
 
 /* _4_BIT, _8_BIT*/
 #define  LCD_MODE  _4_BIT
 
 
-#define  RS   PINB7
-#define  EN   PINB1
-
-#define  LCD_PORT   PA
-
-#define  D7  PINA1
-#define  D6  PINB0
-#define  D5  PINC3
-#define  D4  PINA7
-
-
-
+ 
+extern Dio_ConfigType LCD_PORT  ;
+extern Dio_ConfigType RS ; 
+extern Dio_ConfigType EN ; 
+extern Dio_ConfigType D4 ; 
+extern Dio_ConfigType D5 ; 
+extern Dio_ConfigType D6 ; 
+extern Dio_ConfigType D7 ; 
 
 #endif /* LCD_CFG_H_ */
