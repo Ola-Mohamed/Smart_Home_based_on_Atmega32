ae9a3c4780661a0ee661926d24165f66655d76a9
elaf76 <148910937+elaf76@users.noreply.github.com>
2023-11-02 19:10:24 +0200

    Add files via upload

---
+++ MCAL/Spi/SPI_INTERFACE.h
@@ -0,0 +1,66 @@
+/*
+ * SPI_INTERFACE.h
+ *
+ *  Created on: Nov 1, 2023
+ *      Author: ELAF MOHAMED
+ */
+
+#ifndef SPI_INTERFACE_H_
+#define SPI_INTERFACE_H_
+
+
+/*********** ENABLE SPI OR DISABLE***********/
+#define	SPI_ENABLE		1
+#define	SPI_DISABLE		0
+
+/*********** Interrupt enable or disable***********/
+#define	SPI_INTERRRUPT_ENABLE	1
+#define	SPI_INTERRUPT_DISABLE	0
+
+/*********** put LSB First Or MSB First***********/
+#define SPI_DATA_LSB			0
+#define SPI_DATA_MSB			1
+
+/***********Status of spi master or slave***********/
+#define SPI_MASTER				0
+#define SPI_SLAVE				1
+
+/*********** the numbers which the freq will be divided***********/
+#define SPI_FREQ_DIVIDED_BY_2					0
+#define SPI_FREQ_DIVIDED_BY_4					1
+#define SPI_FREQ_DIVIDED_BY_8					2
+#define SPI_FREQ_DIVIDED_BY_16					3
+#define SPI_FREQ_DIVIDED_BY_32					4
+#define SPI_FREQ_DIVIDED_BY_64					5
+#define SPI_FREQ_DIVIDED_BY_128					6
+
+
+#define SPI_RISING_LEADING_FALLING_TRAILING 	0
+#define SPI_FALLING_LEADING_RISING_TRAILING		1
+
+
+#define SPI_SAMPLE_LEADING_SETUP_TRAILING 		0
+#define SPI_SETUP_LEADING_SAMPLE_TRAILING		1
+
+
+
+typedef struct
+{
+	uint8 SPI_InterruptEnable ;
+	uint8 SPI_SPIEnable ;
+	uint8 SPI_DataOrder ;
+	uint8 SPI_MasterSlaveSelect ;
+	uint8 SPI_ClockPolarity ;
+	uint8 SPI_ClockPhase ;
+	uint8 SPI_ClockRate ;
+}SPI_CONFIG;
+
+
+uint8 SPI_InitConfig (SPI_CONFIG * spi);
+uint8 SPI_Tranceive_Receive (uint8 Copy_TData , uint8 * Copy_RData);
+void SPI_SendByte(uint8 data);
+uint8 SPI_ReceiveByte();
+void SPI_SendString(uint8 *str);
+void SPI_ReceiveString(uint8 *str);
+
+#endif /* SPI_INTERFACE_H_ */
