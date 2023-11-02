eba5bef29a7a087948c333913b4a80ee1191656b
Ola mohamed <Ola180483@feng.bu.edu.eg>
2023-11-02 22:29:59 +0200

    final

--- HAL/Button/Button.c
+++ HAL/Button/Button.c
@@ -14,6 +14,15 @@
 /* Global variable to hold the button state */
 static uint8 g_button_state = BUTTON_RELEASED;
 /***********************************************************************************************************/
+
+/************************************************************************************
+* Service Name: HAL_BUTTON_init
+* Parameters (in): BUTTON_config_t *button_config
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Function to Initialize the Button module.
+************************************************************************************/
 void HAL_BUTTON_init(BUTTON_config_t *button_config) {
 	  // Check if the button configuration is valid.
 	  if (button_config == NULL_PTR) {
@@ -29,6 +38,14 @@ void HAL_BUTTON_init(BUTTON_config_t *button_config) {
 }
 
 /***********************************************************************************************************/
+/************************************************************************************
+* Service Name: HAL_BUTTON_getState
+* Parameters (in): Button_PORT,Button_PIN
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: g_button_state
+* Description: Function to Initialize the Button module.
+************************************************************************************/
 uint8 HAL_BUTTON_getState(Button_PORT,Button_PIN)
 {
 
--- HAL/LED/Led.c
+++ HAL/LED/Led.c
@@ -9,7 +9,14 @@
 
 #include "Led.h"
 
-
+/************************************************************************************
+* Service Name: HAL_LED_init
+* Parameters (in): LED_config_t
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Function to Initialize the LED module.
+************************************************************************************/
 
 
 void HAL_LED_init(LED_config_t Led_config){
@@ -17,19 +24,40 @@ void HAL_LED_init(LED_config_t Led_config){
 	MCAL_DIO_INIT(&Led_config->port_id,&Led_config->pin_id,&Led_config->pin_direction);
 
 }
-
+/************************************************************************************
+* Service Name: HAL_LED_setOn
+* Parameters (in): None
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Function to SET the LED module ON.
+************************************************************************************/
 void HAL_LED_setOn(void)
 {
 	 MCAL_DIO_Std_WRITE_PIN_DIRECTION(LED_PORT,LED_PIN,LED_ON);  /* LED ON */
 }
 
-/*********************************************************************************************/
+/************************************************************************************
+* Service Name: HAL_LED_setOff
+* Parameters (in):None
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Function to SET the LED module Off.
+************************************************************************************/
 void HAL_LED_setOff(void)
 {
 	MCAL_DIO_Std_WRITE_PIN_DIRECTION(LED_PORT,LED_PIN,LED_OFF); /* LED OFF */
 }
 
-/*********************************************************************************************/
+/************************************************************************************
+* Service Name: HAL_LED_toggle
+* Parameters (in): None
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Function to Toggle LED.
+************************************************************************************/
 void HAL_LED_toggle(void)
 {
      MCAL_DIO_Std_FLIP_PIN_DIRECTION(LED_PORT,LED_PIN);
