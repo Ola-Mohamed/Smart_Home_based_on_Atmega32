35f326818ad3883b82d2d03c68b55e4e359dc51b
AhmedGamalEldesawy <ahmedelabd17761@gmail.com>
2023-11-03 23:57:31 +0200

    motion sensor driver

---
+++ HAL/motion_sensor/Motion_Sensor_Interface.h
@@ -0,0 +1,12 @@
+
+
+#ifndef MOTION_SENSOR_INTERFACE_H_
+#define MOTION_SENSOR_INTERFACE_H_
+#include "DIO_INTERFACE.h"
+#include "Motion_Sensor_Confg.h"
+
+DIO_LEVEL_TYPE SENSOR_MotionDetect (void) ; 
+
+
+
+#endif /* MOTION_SENSOR_INTERFACE_H_ */
