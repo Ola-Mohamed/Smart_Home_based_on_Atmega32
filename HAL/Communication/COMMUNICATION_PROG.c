cdbf5a4e45f32d972fc5fb26e91cc1400120f650
magdy_khalifa <gogo.diego101010@gmail.com>
2023-11-06 00:09:01 +0200

    Communication Driver

---
+++ HAL/Communication/COMMUNICATION_PROG.c
@@ -0,0 +1,30 @@
+﻿/*
+ * COMMUNICATION_PROG.c
+ *
+ * Created: 04/11/2023 12:50:43 م
+ *  Author: JoBa
+ */ 
+
+/*   */
+
+#include "COMMUNICATION_INTERFACE.h"
+
+uint8 COMMUNICATION_recieveChar(){
+	uint8 dataByte ; 
+	dataByte = UART_recieveByte();
+	return dataByte;
+}
+
+void COMMUNICATION_receiveString(uint8 *Str){
+	
+	 UART_receiveString(Str);
+
+}
+
+void COMMUNICATION_sendString(const uint8 *Str){
+	
+	 UART_sendString(Str);
+
+}
+
+
