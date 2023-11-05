6d78f1c083800220db07d1b3c55a572acd97d4ab
AhmedGamalEldesawy <ahmedelabd17761@gmail.com>
2023-11-06 00:09:04 +0200

    MOTOR_DC_DRIVER

---
+++ HAL/MOTOR_DC/Mototr_DC_Con.h
@@ -0,0 +1,19 @@
+/*
+ * Mototr_DC_Con.h
+ *
+ * Created: 11/5/2023 11:06:53 PM
+ *  Author: ahmed
+ */ 
+
+
+#ifndef MOTOTR_DC_CON_H_
+#define MOTOTR_DC_CON_H_
+#include "MemMap.h"
+#include "DIO_TYPES.h"
+
+extern Dio_ConfigType  M1_IN1   ;
+extern Dio_ConfigType  M1_IN2   ;
+extern Dio_ConfigType  M2_IN1  ;
+extern Dio_ConfigType  M2_IN2   ;
+
+#endif /* MOTOTR_DC_CON_H_ */
---
+++ HAL/MOTOR_DC/Mototr_DC_Confg.c
@@ -0,0 +1,7 @@
+#include "DIO_TYPES.h"
+
+Dio_ConfigType  M1_IN1 = {PIN0_ID , PORTD_ID  }  ;
+Dio_ConfigType  M1_IN2 = {PIN1_ID , PORTD_ID }  ;
+
+Dio_ConfigType  M2_IN1 = {PIN2_ID , PORTD_ID  }  ;
+Dio_ConfigType  M2_IN2 = {PIN3_ID , PORTD_ID}  ;
