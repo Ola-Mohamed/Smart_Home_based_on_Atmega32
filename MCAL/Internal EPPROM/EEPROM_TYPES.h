9d7acc9eef3f4ae050d245a3a2ae30a5de6a18ee
Ola-Mohamed <Ola180483@feng.bu.edu.eg>
2023-11-07 23:30:58 +0200

    make folders

---
+++ MCAL/Internal EPPROM/EEPROM_TYPES.h
@@ -0,0 +1,17 @@
+/*
+ * EEPROM_TYPES.h
+ *
+ *  Created on: Nov 2, 2023
+ *      Author: hp
+ */
+
+#ifndef EEPROM_TYPES_H_
+#define EEPROM_TYPES_H_
+/*******************************************************************************
+ *                      Preprocessor Macros                                    *
+ *******************************************************************************/
+#define ERROR 0
+#define SUCCESS 1
+
+
+#endif /* EEPROM_TYPES_H_ */
---
+++ MCAL/Internal EPPROM/readme.txt
@@ -0,0 +1,2 @@
+file hh
+hh
